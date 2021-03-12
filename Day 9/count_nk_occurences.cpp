f89aa681900779ec05e11aad595d32ab79e0bc1c
rajatrocker3315 <20818412+rajatrocker3315@users.noreply.github.com>
2021-03-12 19:17:31 +0530

    Day 9

---
+++ Day 9/count_nk_occurences.cpp
@@ -0,0 +1,47 @@
+// { Driver Code Starts
+// A C++ program to print elements with count more than n/k
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int countOccurence(int arr[], int n, int k);
+// Position this line where user code will be pasted.
+
+/* Driver program to test above function */
+int main() {
+    int t, k;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+
+        int arr[n];
+
+        for (i = 0; i < n; i++) cin >> arr[i];
+        cin >> k;
+        cout << countOccurence(arr, n, k) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+
+
+
+// Function to find elements with count more than n/k times
+// arr: input array
+int countOccurence(int arr[], int n, int k) {
+    unordered_map<int, int> m;
+    
+    int i=0, c=0;
+    for(i=0 ;i<n;i++)
+      m[arr[i]]++;
+    
+    for(auto x:m)
+    {
+        if(x.second > (n/k))
+           c++;
+    }
+    
+    return c;
+}
